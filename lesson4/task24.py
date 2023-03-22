c9d026bfcd3a7265d0672053fa5bf84a65f924e3
zoom Дмитрий <z-oo-m@yandex.ru>
2023-03-23 00:18:49 +0100

    Задача 24. Файлы задач переименованы, была ошибка по номерам

--- lesson4/task24.py
+++ lesson4/task24.py
@@ -1,6 +1,7 @@
 '''
-Вывод упорядоченных чисел взятых из двух множеств, состоящих из неупорядоченных наборов чисел
+Нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль
 '''
+from random import randrange
 
 
 def input_int(out_txt, digit=None):
@@ -12,29 +13,18 @@ def input_int(out_txt, digit=None):
     return digit
 
 
-def input_list(out_txt, num, string_in=None):
-    while string_in is None or len(string_in) != num or res is False:
-        string_in = input(out_txt, )
-        string_in = string_in.split()
-        res = all(map(str.isdigit, string_in))
-    return string_in
+def func_score(bush_in):
+    berries = []
+    for i in range(bush_in):
+        berries.append(randrange(1, bush_in))
+    print(f'Ягод на кустах: {berries}')
+    i, three_bush = 0, 0
+    three_bush = max(three_bush, berries[i] + berries[int(bush_in) - 1] + berries[int(bush_in) - 2])
+    three_bush = max(three_bush, berries[i] + berries[i + 1] + berries[int(bush_in) - 1])
+    for i in range(bush_in - 2):
+        three_bush = max(three_bush, berries[i] + berries[i + 1] + berries[i + 2])
+    return three_bush
 
 
-def func_join(list_in1, list_in2):
-    list_in1.extend(list_in2)
-    list_in1 = list(set(list_in1))
-    list_in1 = sorted(list_in1, key=lambda x: int(x[0:]))
-    return list_in1
-
-
-print('Вывод упорядоченных чисел взятых из двух множеств, состоящих из неупорядоченных наборов чисел')
-print()
-# tt = [5, 3, 6, 7, 8, 23, 34, 51, 55, 71, 3, 23, 6, 71]
-# print(tt)
-# print(sorted(tt))
-number1 = input_int('Ведите размер первого списка чисел: ')
-number2 = input_int('Введите размер второго списка чисел: ')
-list1 = input_list('Введите, через пробел, набор чисел для первого множества: ', number1)
-list2 = input_list('Введите, через пробел, набор чисел для второго множества: ', number2)
-
-print(f'Объединенные неповторяющиеся числа:', func_join(list1, list2))
+num_bush = input_int('Ведите целое количество кустов: ')
+print(f'Максимальное количество ягод собранных за один раз:', func_score(num_bush))
